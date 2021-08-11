996ab1434ec2b3c638db39a288d6dc380d454606
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 01:33:42 +0300

    created a method that helps to define properties for our objects.

--- password.py
+++ password.py
@@ -1,6 +1,14 @@
 class User:
     """
-    a class that generates new instances of passwors
+    a class that generates new instances of passwords
     """
 
     user_list = []
+
+     def __init__(self,first_name,last_name,password):
+        """
+        _init_ a method that helps to define properties for our objects.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
