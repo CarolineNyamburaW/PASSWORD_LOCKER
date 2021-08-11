33ffd647cedbbda0cb85ddd5e5570d6c5632fbb2
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 01:54:18 +0300

    created a method that takes in username and returns a username that matches the username

--- password.py
+++ password.py
@@ -75,3 +75,12 @@ class Credential:
         saves credentail objects into credential list
         """
         Credential.credential_list.append(self)
+
+    @classmethod
+    def find_credential_by_username(cls,username):
+        """
+        this method takes in username and returns a username that matches the username
+        """
+        for credential in cls.credential_list:
+            if credential.username == username:
+                return credential
