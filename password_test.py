4be55b6e1a8570c96b5b90ab0342ecf950ca7c26
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 02:07:51 +0300

    created a class TestPassword(unittest.TestCase):

--- password_test.py
+++ password_test.py
@@ -1,3 +1,17 @@
 import unittest
 from password import User
 from password import Credential
+
+class TestPassword(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = User("carol","nyambu","b243#$5")
+
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name,"carol")
+        self.assertEqual(self.new_user.last_name,"nyambu")
+        self.assertEqual(self.new_user.password,"b243#$5")
+
+
+
+
