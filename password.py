926acb1f8b3baa7bb6e3f1b85bfe9269ade82fdd
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 01:40:13 +0300

    created a method that deletes a user from the user_list

--- password.py
+++ password.py
@@ -5,7 +5,7 @@ class User:
 
     user_list = []
 
-     def __init__(self,first_name,last_name,password):
+    def __init__(self,first_name,last_name,password):
         """
         _init_ a method that helps to define properties for our objects.
         """
@@ -13,19 +13,26 @@ class User:
         self.last_name = last_name
         self.password = password
 
-      def save_user(self):
+    def save_user(self):
          """
          this method saves contact objects into user_list
          """
 
          User.user_list.append(self)  
 
-      @classmethod
-      def find_user_by_password(cls,password):
+    @classmethod
+    def find_user_by_password(cls,password):
         """
         this method takes in a password and returns a password that matches tha password.
         """
 
         for user in cls.user_list:
             if user.password == password:
-                return user   
+                return user  
+
+    def delete_user(self):
+        """
+        this method deletes a user from the user_list
+        """
+
+        User.user_list.remove(self) 
