eee21e056a52742749d8fb7a71e0be3020c21a71
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 01:35:34 +0300

    created a method saves contact objects into user_list

--- password.py
+++ password.py
@@ -12,3 +12,10 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
+      def save_user(self):
+         """
+         this method saves contact objects into user_list
+         """
+
+         User.user_list.append(self)     
