a5822582c285bb05e060fa64a58c41798f0cef35
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 02:10:11 +0300

    created a method that tests if the user object is saved into the userlist

--- password_test.py
+++ password_test.py
@@ -12,6 +12,12 @@ class TestPassword(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"nyambu")
         self.assertEqual(self.new_user.password,"b243#$5")
 
+    def test_save_user(self):
+            """
+            this method tests if the user object is saved into the userlist
+            """
+            self.new_user.save_user() # saves the new user
+            self.assertEqual(len(User.user_list),1)
 
 
 
