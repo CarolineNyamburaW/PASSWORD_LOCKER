f9c8acc7d2a177d57da4036ea6b3eeee52e96ea9
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-08-12 13:20:31 +0300

    created a method that tests if the credential is saved into the credentiallist

--- password_test.py
+++ password_test.py
@@ -81,5 +81,14 @@ class TestCredential(unittest.TestCase):
 
     def setUp(self):
         self.new_credential = Credential("Account","br254","nims67&")
+    
+    def test_unit(self):
+        """
+        this method tests if the credential is saved into the credentiallist
+        """
+        self.assertEqual(self.new_credential.Account,"Account")
+        self.assertEqual(self.new_credential.username,"br254")
+        self.assertEqual(self.new_credential.password,"nims67&")
+
         
    
